c8b3aa150c0b448a63406d2123d326f231bc3daf
motya <motya0414@hotmail.co.jp>
2020-04-29 00:53:35 +0900

    [update] バージョン

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="random_string",  # これがパッケージ名になります～
-    version="0.0.1",
+    version="0.0.2",
     packages=['random_string'],  # moduleが入っているパッケージを選択してください～
     entry_points={
         'console_scripts': ['random_string = random_string:main'],
