a1747d8faf0ac590e87be7c4cb5173e1de89ddc0
motya <motya0414@hotmail.co.jp>
2020-04-29 00:53:22 +0900

    [update] OSのコマンドとして登録

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setuptools.setup(
     name="random_string",  # これがパッケージ名になります～
     version="0.0.1",
     packages=['random_string'],  # moduleが入っているパッケージを選択してください～
+    entry_points={
+        'console_scripts': ['random_string = random_string:main'],
+    },
 )
